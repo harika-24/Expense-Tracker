9c1ce054ec1f907bf03a2b8b3cf9ab0a7a397880
harika-24 <harika.reddy2401@gmail.com>
2022-08-19 14:56:16 -0400

    Added the ExpenseItems and cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import ExpenseCard from './components/ExpensesCard';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', 
+    title: 'Fruits', 
+    amount: 10.94, 
+    date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'Book Shelf (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get Started!!</h2>
+      <ExpenseCard items={expenses}/>
     </div>
   );
 }
