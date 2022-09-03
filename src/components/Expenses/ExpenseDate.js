7b69a3a1bda7e83943010ec5093f5ee7e65ed91d
harika-24 <harika.reddy2401@gmail.com>
2022-09-02 21:24:16 -0400

    Added the form

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -4,10 +4,10 @@ function ExpenseDate(props) {
     return (
         <div className='expense-date'>
 
-<div className='expense-date__day'>{props.date.toLocaleString('en-US', { day: 'numeric' })}</div>
+            <div className='expense-date__day'>{props.date.toLocaleString('en-US', { day: 'numeric' })}</div>
             <div className='expense-date__month'>{props.date.toLocaleString('en-US', { month: 'long' })}</div>
             <div className='expense-date__year'>{props.date.toLocaleString('en-US', { year: 'numeric' })}</div>
-            
+
         </div>
     );
 
